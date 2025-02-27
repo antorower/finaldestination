d31e4838b05eb130dc26663bbe2d8da625a72c41
antorower <antonis.mastorakis@gmail.com>
2025-02-27 12:12:40 +0200

    try

--- app/admin/accounts/page.js
+++ app/admin/accounts/page.js
@@ -2,11 +2,6 @@ import dbConnect from "@/dbConnect";
 import Account from "@/models/Account";
 import { revalidatePath } from "next/cache";
 
-const GetAccounts = async () => {
-  "use server";
-  return null;
-};
-
 const Accounts = () => {
   return <div>content</div>;
 };
