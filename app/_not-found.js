1099712f89177d700064a791ba43e59fe9d09f4c
antorower <antonis.mastorakis@gmail.com>
2025-02-27 12:24:02 +0200

    good

---
+++ app/_not-found.js
@@ -0,0 +1,7 @@
+export const dynamic = "force-dynamic";
+
+const NotFound = () => {
+  return <div>Η σελίδα που ψάχνεις δεν υπάρχει</div>;
+};
+
+export default NotFound;
--- app/admin/page.js
+++ app/admin/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import AdminMenuItem from "./AdminMenuItem";
 import PageTransition from "@/components/PageTransition";
 
