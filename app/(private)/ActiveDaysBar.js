0d127230c81369e61688cb76fb9d47b35912dbcf
antorower <antonis.mastorakis@gmail.com>
2025-03-23 05:03:17 +0200

    correct active days

--- app/(private)/ActiveDaysBar.js
+++ app/(private)/ActiveDaysBar.js
@@ -2,10 +2,10 @@ const ActiveDaysBar = ({ mon, tue, wed, thu, fri }) => {
   return (
     <div className="flex font-bold items-center gap-4 px-4 py-2 bg-gray-50 justify-center rounded border border-gray-300 text-gray-600 text-sm">
       <Day day="ΔΕΥ" active={mon} />
-      <Day day="ΤΡΙ" active={mon} />
-      <Day day="ΤΕΤ" active={mon} />
-      <Day day="ΠΕΜ" active={mon} />
-      <Day day="ΠΑΡ" active={mon} />
+      <Day day="ΤΡΙ" active={tue} />
+      <Day day="ΤΕΤ" active={wed} />
+      <Day day="ΠΕΜ" active={thu} />
+      <Day day="ΠΑΡ" active={fri} />
     </div>
   );
 };
