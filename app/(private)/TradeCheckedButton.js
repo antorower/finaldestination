b8b630f709cfc15f023a0d2b05ff5e9c6ba11e04
antorower <antonis.mastorakis@gmail.com>
2025-03-26 09:25:23 +0200

    trade sub

--- app/(private)/TradeCheckedButton.js
+++ app/(private)/TradeCheckedButton.js
@@ -1,15 +1,20 @@
 "use client";
 import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 const TradeCheckedButton = ({ TradeChecked, tradeId, userId, accountId }) => {
+  const router = useRouter();
+
   const Checked = async () => {
     const response = await TradeChecked({ tradeId, userId, accountId });
     if (response.error) {
       toast.error(response.message);
     } else {
       toast.success(response.message);
+      router.push("/");
     }
   };
+
   return (
     <button onClick={Checked} className="col-span-6 p-2 mt-2 lg:mt-0 lg:col-span-1 flex justify-center items-center bg-green-500 text-white font-bold text-3xl">
       ✔
