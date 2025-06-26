f035962f00b8121a4622f710b2e840568e87f9e0
antorower <antonis.mastorakis@gmail.com>
2025-06-26 09:36:43 +0300

    after problem not solved

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/api/(.*)", "/education"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/api/(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
   const url = new URL(request.url);
