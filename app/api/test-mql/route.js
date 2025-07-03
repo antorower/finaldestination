6fec336d87bf0d62ab4e7d143166e22da63d0d5b
antorower <antonis.mastorakis@gmail.com>
2025-07-03 17:52:27 +0300

    correct

--- app/api/test-mql/route.js
+++ app/api/test-mql/route.js
@@ -1,6 +1,10 @@
 export async function GET(req) {
   try {
     console.error("Web Request!!!");
+    const { symbol, lots } = req.query;
+    console.log("Symbol ", symbol);
+    console.log("Lots ", lots);
+
     return Response.json({ success: true });
   } catch (error) {
     console.error("Assistant error:", error);
