a20fbbbb6dfb44f73d1d95a598ef851b028f3cd6
antorower <antonis.mastorakis@gmail.com>
2025-07-03 18:19:52 +0300

    new request

--- app/api/test-mql/route.js
+++ app/api/test-mql/route.js
@@ -1,13 +1,16 @@
 export async function POST(req) {
   try {
-    const { symbol } = await req.json();
+    // const { symbol } = await req.json();
+    // const { messages } = await req.json();
+    // console.log(req);
+    // console.log("Web Request!!!");
+    // console.log("Symbol ", symbol);
+    // console.log("Lots ", lots);
 
-    console.log(req);
-    console.log("Web Request!!!");
-    console.log("Symbol ", symbol);
-    console.log("Lots ", lots);
+    const data = await req.json();
+    console.log("Web Request!!!", data);
 
-    return Response.json({ symbol: symbol });
+    return Response.json({ success: "dfs" });
   } catch (error) {
     console.error("Assistant error:", error);
     return Response.json({ success: false }, { status: 500 });
